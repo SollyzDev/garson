c0b18ef84683871810a9fa17afd3323c073a5180
Eslam Mostafa <cseslam@gmail.com>
2016-03-23 06:12:53 +0200

    init

---
+++ main.go
@@ -0,0 +1,5 @@
+package main
+
+func New() Router {
+  return Router{}
+}
---
+++ router.go
@@ -0,0 +1,35 @@
+package main
+
+import "net/http"
+
+type Router struct {
+  Routes []Route
+}
+
+type Route struct {
+  Method string
+  Path string
+  Handler HandlerFunc
+}
+
+type HandlerFunc func(res http.ResponseWriter, req *http.Request)
+
+func (r Router) Get(path string, handler HandlerFunc) {
+  route := Route{}
+  route.Method = "GET"
+  route.Path = path
+  route.Handler = handler
+  r.Routes.Append(route)
+}
+
+func (r Router) Post(route string) {
+
+}
+
+func (r Router) Put(route string) {
+
+}
+
+func (r Router) Delete(route string) {
+
+}
