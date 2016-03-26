e61f600c56722e5d5e4d285a87f701b89c7922ba
Eslam Mostafa <eslam@eslammostafa.com>
2016-03-26 04:23:56 +0200

    fix New() to return a router

--- router.go
+++ router.go
@@ -2,23 +2,36 @@ package garson
 
 import "net/http"
 
+// Router struct
 type Router struct {
     Routes []Route
 }
 
+// Route struct just contains the method, path and Handle func
 type Route struct {
     Method string
     Path string
-    Handler HandlerFunc
+    Handler Handle
 }
 
-type HandlerFunc func(req *http.Request, res http.ResponseWriter)
+// Handle serves as a type of the func that is qoing to be fired 
+// when the routers finds the requested route
+type Handle func(req *http.Request, res http.ResponseWriter)
 
-func New() Router {
-    return Router{}
+// New creates and return a new router object
+// it should be passed to http.ListenAndServe
+// example:
+// router := garson.New()
+// router.Get("/hello", func(req *http.Request, res http.ResponseWriter){})
+// http.ListenAndServe(":8080", router)
+func New() *Router {
+    return &Router{}
 }
 
-func (r *Router) Try(path string, method string) (*Route,string) { // FIX: should return (*Router,error)
+// Try loops through the routes array to find the requested route
+// If the route is not found, it returns NotFound error
+// FIX: should return (*Router,error)
+func (r *Router) Try(path string, method string) (*Route,string) { 
     for _, route := range r.Routes {
         if route.Method == method && route.Path == path {
             return &route, ""
@@ -27,30 +40,41 @@ func (r *Router) Try(path string, method string) (*Route,string) { // FIX: shoul
     return &Route{}, "route not found"
 }
 
-func (r *Router) Get(path string, handler HandlerFunc) {
+// a shortcut func to append new routes to the routes array
+func add(r *Router, method string, path string, handler Handle) {
     route := Route{}
-    route.Method = "GET"
+    route.Method = method
     route.Path = path
     route.Handler = handler
     r.Routes = append(r.Routes, route)
 }
 
-func (r *Router) Post(route string) {
-
+// Get the adds a GET method to routes
+func (r *Router) Get(path string, handler Handle) {
+    add(r, "GET", path, handler)
 }
 
-func (r *Router) Put(route string) {
-
+// Post the adds a POST method to routes
+func (r *Router) Post(path string, handler Handle) {
+    add(r, "POST", path, handler)
 }
 
-func (r *Router) Delete(route string) {
-
+// Put the adds a PUT method to routes
+func (r *Router) Put(path string, handler Handle) {
+    add(r, "PUT", path, handler)
 }
 
+// Delete the adds a DELETE method to routes
+func (r *Router) Delete(path string, handler Handle) {
+    add(r, "DELETE", path, handler)
+}
+
+// ServeHTTP implementats of the http.Handler interface
 func (r *Router) ServeHTTP(w http.ResponseWriter, req *http.Request) {
     route, err :=  r.Try(req.URL.Path, req.Method)
     if err != "" {
-        panic(err)
+        //panic(err)
+        return
     }
     route.Handler(req, w)
 }
