3ee6709d253d84aff84c07335bdd557b05f83eb8
Islam Shehata <i.shehata@gastrofix.com>
2018-09-30 14:50:15 +0200

    add concourse pipeline conf

---
+++ ci/test.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+echo "go path is" $GOPATH
+echo $(pwd)
+echo $(ls code)
+p=$GOPATH/src/github.com/ishehata/garson
+mkdir -p $p
+cp -R code/* $p/
+cd $p
+echo "verify that code files are copied"
+echo "pwd is $(pwd)"
+echo $(ls)
+echo "installing dependencies"
+apk update && apk add --no-cache git
+go get
+echo "prepairing to start go test"
+go test -v .
